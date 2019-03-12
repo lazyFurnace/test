0edbf9ae05af0b537fa4d4876fad7a0b4d0c1eb9
李齐 <787139384@qq.com>
2019-03-13 01:33:46 +0800

    开始简单拖拽功能

--- Drag/src/drag.js
+++ Drag/src/drag.js
@@ -1,3 +1,4 @@
+/*
 var start_X, start_Y, posi;
 var tar = document.getElementById("mouse");
 tar.onmousedown = function(e) {
@@ -21,3 +22,34 @@ tar.addEventListener("touchstart", function(e) {
 tar.addEventListener("touchmove", function(e) {
   tar.style.marginLeft = parseInt(posi) + e.touches[0].pageX - start_X + "px";
 });
+*/
+
+function drag(elem) {
+  let params = {};
+  if (elem instanceof HTMLElement) {
+    params = {
+      offsetX: null,
+      offsetY: null
+    };
+    elem.style.position = "absolute";
+    elem.style.left = "0px";
+    elem.style.top = "0px";
+    elem.addEventListener("mousedown", e => {
+      params.offsetX = e.offsetX;
+      params.offsetY = e.offsetY;
+      document.addEventListener("mousemove", dragMouseMove);
+    });
+    elem.addEventListener("mouseup", e => {
+      document.removeEventListener("mousemove", dragMouseMove);
+    });
+    elem.addEventListener("mouseout", e => {
+      document.removeEventListener("mousemove", dragMouseMove);
+    });
+  } else {
+    throw new Error("绑定元素非 DOM 元素。")
+  }
+  function dragMouseMove(e) {
+    elem.style.left = e.pageX - params.offsetX + "px";
+    elem.style.top = e.pageY - params.offsetY + "px";
+  }
+}
